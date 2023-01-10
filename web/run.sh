55f718ebde28e7732a5e3145686adb4f05616c3b
Jan Prochazka <prochazka@j.end.as>
2023-01-10 23:53:29 +0100

    added service autorestart

--- web/run.sh
+++ web/run.sh
@@ -5,4 +5,4 @@ docker build -t autogeo "$(dirname "$(realpath "$0")")"
 docker stop autogeo
 docker rm autogeo
 echo "running autogeo"
-docker run --detach --publish 127.0.0.1:17859:17859 --cap-drop ALL --cap-add DAC_OVERRIDE --log-opt max-size=10m --log-opt max-file=1 --name autogeo --rm autogeo
+docker run --detach --publish 127.0.0.1:17859:17859 --cap-drop ALL --cap-add DAC_OVERRIDE --log-opt max-size=10m --log-opt max-file=1 --name autogeo --restart unless-stopped autogeo
